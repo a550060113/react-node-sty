0be5e8f8e9696056aa5c3ee1fa55d323249fee73
a550060113 <550060113@qq.com>
2026-04-14 01:59:10 +0800

    修改了菜单栏

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,47 +1,34 @@
-import {useState} from 'react';
+import { useState, useEffect } from 'react';
 import { Menu } from 'antd';
-import {asyncRouters} from "@/router/routerConfig.jsx";
-import {useNavigate} from 'react-router-dom'
-import {filterHiddenRoutes,filterMenus,filterPathArray} from '@/utils/filterRoutes.jsx'
-import {useSelector,useDispatch} from "react-redux";
+import { asyncRouters } from "@/router/routerConfig.jsx";
+import { useNavigate, useLocation } from 'react-router-dom'
+import { filterHiddenRoutes, filterMenus, filterPathArray } from '@/utils/filterRoutes.jsx'
 
-function SideMenu({collapsed}) {
-    // const [items ,setItems ] = useState([])
+function SideMenu({ collapsed }) {
     const [tooltipEnabled, setTooltipEnabled] = useState(true);
-    const {menuList} = useSelector(state => state.menu);
-    const dispatch = useDispatch();
-    //初始化选中菜单
     const navigate = useNavigate();
+    const location = useLocation();
 
+    const arr = filterHiddenRoutes(asyncRouters);
+    const items = filterMenus(arr);
 
-    let arr = filterHiddenRoutes(asyncRouters)
-    console.log('arr',arr)
-    let items = []
-    items = filterMenus(arr)
-    console.log('item',items)
-    // if(menuList.length == 0){
-    //     dispatch()
-    // }
-    let filterPathArr = filterPathArray(window.location.pathname,items).reverse()
+    const filterPathArr = filterPathArray(location.pathname, items).reverse();
+    const selectedKeys = filterPathArr.length > 0 ? [filterPathArr[0]] : [];
 
-    let defaultSelectedKeys = []
-    let defaultOpenKeys = []
-    if(filterPathArr.length == 1){
-        defaultSelectedKeys = filterPathArr
-        defaultOpenKeys = filterPathArr
-    }else if(filterPathArr.length > 1){
-        defaultSelectedKeys = [filterPathArr[0]]
-        defaultOpenKeys = filterPathArr
-    }
-    // 回显菜单当前高亮
-    // let defaultSelectedKeys = localStorage.getItem('defaultSelectedKeys') ? [localStorage.getItem('defaultSelectedKeys')] :[items[1].key]
-    // let defaultOpenKeys = localStorage.getItem('defaultOpenKeys') ? JSON.parse(localStorage.getItem('defaultOpenKeys')) :[]
-    console.log('defaultSelectedKeys',defaultSelectedKeys)
-    console.log('defaultOpenKeys',defaultOpenKeys)
+    // 👇 展开状态自己管理
+    const [openKeys, setOpenKeys] = useState([]);
 
-// 点击菜单导航
-    function onclickItem({ key, keyPath }){
-        // 保存菜单的key
+    // 路由变化时自动展开对应菜单
+    useEffect(() => {
+        setOpenKeys(filterPathArr);
+    }, [location.pathname]);
+
+    // 点击展开/关闭时同步状态
+    const handleOpenChange = (keys) => {
+        setOpenKeys(keys);
+    };
+
+    const onclickItem = ({ key, keyPath }) => {
         localStorage.defaultSelectedKeys = key
         localStorage.defaultOpenKeys = JSON.stringify(keyPath)
         let path = keyPath.reverse().join('/')
@@ -50,19 +37,22 @@ function SideMenu({collapsed}) {
         console.log('点击的keyPath',keyPath)
         console.log('点击的path',path)
         navigate(path)
-        // console.log(key,keyPath)
-    }
+    };
+
     return (
-            <Menu
-                defaultSelectedKeys={defaultSelectedKeys}
-                defaultOpenKeys={defaultOpenKeys}
-                mode="inline"
-                theme="light"
-                onClick={onclickItem}
-                inlineCollapsed={collapsed}
-                tooltip={tooltipEnabled ? { placement: 'left' } : false}
-                items={items}
-            />
+        <Menu
+            // 👇 只留这三个！！！
+            selectedKeys={selectedKeys}
+            openKeys={openKeys}
+            onOpenChange={handleOpenChange}
+
+            mode="inline"
+            theme="light"
+            onClick={onclickItem}
+            inlineCollapsed={collapsed}
+            tooltip={tooltipEnabled ? { placement: 'left' } : false}
+            items={items}
+        />
     );
 }
 
--- src/page/Admin/NewAdmin.jsx
+++ src/page/Admin/NewAdmin.jsx
@@ -2,14 +2,19 @@ import React, {useEffect} from 'react';
 import styles from '././NewAdmin.module.css';
 import AddminForm from "@/page/Admin/components/AddminForm.jsx";
 import {useSelector,useDispatch} from "react-redux";
+import {useNavigate} from "react-router-dom";
 import {addAdminAsyncThunk,getAdminListAsyncThunk} from "@/redux/adminSlice.js";
+import {message} from "antd";
 
 function NewAdmin() {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const {adminList} = useSelector((state) => state.admin);
 
     const onsubmit = (formValues) =>{
         dispatch(addAdminAsyncThunk(formValues))
+        message.success('添加成功')
+        navigate('/admin/admin-list')
         console.log('formValues',formValues);
     }
 
